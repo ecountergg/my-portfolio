d0abde277264aaea722f0526e15cd679f8abd916
Eka Yuda Rayi Anugrah <eka.yuda@nostratech.com>
2023-09-10 02:19:13 +0700

    deps: update package @nuxtjs/i18n

--- configs/i18n.config.ts
+++ configs/i18n.config.ts
@@ -3,7 +3,6 @@ import { NuxtI18nOptions } from "@nuxtjs/i18n/dist/module";
 import { locales } from "../configs/locales-i18n.config";
 
 export const i18n: NuxtI18nOptions = {
-  lazy: true,
   langDir: "locales",
   defaultLocale: "en",
   strategy: "no_prefix",
