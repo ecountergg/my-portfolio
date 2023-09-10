e2f1faa2b8e5085083b0d835904a266b36471c1d
Eka Yuda Rayi Anugrah <eka.yuda@nostratech.com>
2023-09-10 15:22:40 +0700

    feat(components): create component tag

---
+++ components/atoms/Tag/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <span
+    class="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-quaternary text-primary rounded-full"
+  >
+    <icon v-if="icon" :name="icon" class="mr-2 w-6 h-6" />
+    {{ label }}
+  </span>
+</template>
+<script lang="ts">
+export default {
+  name: "ATag",
+  inheritAttrs: false,
+};
+</script>
+<script setup lang="ts">
+interface Props {
+  label: string;
+  icon: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  icon: "",
+});
+</script>
