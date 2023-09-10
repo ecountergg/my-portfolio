59f74a6325ef14d89baaad5d5ce332788cd40353
Eka Yuda Rayi Anugrah <eka.yuda@nostratech.com>
2023-09-10 20:34:37 +0700

    feat: create favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,35 @@ import { i18n } from "./configs/i18n.config";
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "Eka Yuda Rayi Anugrah",
       charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/favicon/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon/favicon-16x16.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon/favicon-32x32.png",
+        },
+        {
+          rel: "manifest",
+          href: "/favicon/site.webmanifest",
+        },
+      ],
       meta: [
         { name: "description", content: "Eka Yuda Rayi Anugrah - Profile" },
       ],
+      title: "Eka Yuda Rayi Anugrah",
+      viewport: "width=device-width, initial-scale=1",
     },
   },
   plugins: [{ src: "~/plugins/scroll-behavior.ts", mode: "client" }],
