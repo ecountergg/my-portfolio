86958d98a827f3f18972eb4e615281b7ec97cf67
Eka Yuda Rayi Anugrah <eka.yuda@nostratech.com>
2023-09-10 15:21:54 +0700

    feat: setup iconify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,18 @@ import { i18n } from "./configs/i18n.config";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Eka Yuda Rayi Anugrah",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [
+        { name: "description", content: "Eka Yuda Rayi Anugrah - Profile" },
+      ],
+    },
+  },
   plugins: [{ src: "~/plugins/scroll-behavior.ts", mode: "client" }],
-  modules: ["@nuxtjs/i18n"],
+  modules: ["@nuxtjs/i18n", "nuxt-icon"],
   i18n,
   postcss: {
     plugins: {
