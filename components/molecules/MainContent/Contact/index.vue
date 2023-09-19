0a0158f11d2341928e9b738e18a335e015d0eeb3
Eka Yuda Rayi Anugrah <eka.yuda@nostratech.com>
2023-09-19 21:03:57 +0700

    fix(locale): translation id

--- components/molecules/MainContent/Contact/index.vue
+++ components/molecules/MainContent/Contact/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <MSection id="contact" :title="$t('headers.contact')"> </MSection>
+  <MSection id="contact" :title="$t('headers.contact')">
+    <div class="pl-10 text-primary">
+      <p>{{ $t("contact-desc") }}</p>
+    </div>
+  </MSection>
 </template>
 <script lang="ts">
 export default {
